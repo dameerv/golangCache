1df5c02c4eded38023bed7ded407baec1289c423
Damir Vakhitov <d2450170@yandex.ru>
2022-07-28 04:41:38 +0300

    refactoring after review

--- cache.go
+++ cache.go
@@ -1,8 +1,12 @@
 package cache
 
-import "errors"
+import (
+	"errors"
+	"sync"
+)
 
 type Cache struct {
+	mu sync.RWMutex
 	cache map[string]interface{}
 }
 
@@ -12,20 +16,29 @@ func New() *Cache {
 	}
 }
 func (c *Cache) Set(key string, value interface{}) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.cache[key] = value
 }
 
 func (c Cache) Get(key string) (interface{}, error) {
+	c.mu.RLock()
 	if _, ok := c.cache[key]; !ok {
 		return nil, errors.New("key not found")
 	}
+
+	defer c.mu.RUnlock()
 	return c.cache[key], nil
 }
 
 func (c *Cache) Delete(key string) error {
+	c.mu.RLock()
+	
 	if _, ok := c.cache[key]; !ok {
 		return errors.New("can't delete: key not found")
 	}
+
+	defer c.mu.Unlock()
 	delete(c.cache, key)
 	return nil
 }
